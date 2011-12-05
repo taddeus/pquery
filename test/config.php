be0b6542054a09725be67098c38e9363ad2d05e6
Taddeus Kroes <taddeuskroes@hotmail.com>
2011-12-05 17:11:39 +0100

    Removed commented code from test index.

---
+++ test/config.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Base and plugin config for pQuery utility framework.
+ * 
+ * @package pQuery
+ */
+
+/**
+ * Indicates whether the framework is in debug mode, which right
+ * now only means that occuring errors will be displayed or not.
+ * 
+ * @var bool
+ */
+define('DEBUG', true);
+
+/**
+ * The root location of the pQuery framework folder, used for file
+ * inclusions. The path is absolute and should end with a slash ('/').
+ * 
+ * @var string
+ */
+define('PQUERY_ROOT', 'D:/xampp/htdocs/pquery/');
+
+/**
+ * Abstract class containing plugin configs.
+ */
+abstract class pQueryConfig {
+	/**
+	 * Indicates whether errors occuring in the framework will
+	 * terminate the script.
+	 * 
+	 * @var bool
+	 */
+	const ERROR_IS_FATAL = true;
+}
+
+?>
--- test/index.php
+++ test/index.php
@@ -1,7 +1,7 @@
 <?php
 
 include_once '../../simpletest/autorun.php';
-include_once '../pquery.config.php';
+include_once 'config.php';
 include_once PQUERY_ROOT.'pquery.php';
 
 function is_test_file($filename) {
@@ -10,17 +10,5 @@ function is_test_file($filename) {
 
 foreach( array_filter(scandir('.'), 'is_test_file') as $file )
 	include_once $file;
-
-//include_once '../pquery.php';
-//__p::require_plugins('array', 'sql', 'template');
-
-// SQL test
-/*include_once '../../debug.php';
-$sql = _sql("select * from posts where slug = '[slug]'")
-		->set(array('slug' => 'contact'));
-$results = $sql->fetch_all('object');
-$results = _arr($results);
-
-debug($results);*/
 
 ?>
