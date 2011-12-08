650becb32839aefe30e7ab16d0626734e0b07a5b
Taddeus Kroes <taddeuskroes@hotmail.com>
2011-12-08 23:53:06 +0100

    Added test skeleton for pQuery base class tests.

--- test/base.php
+++ test/base.php
@@ -3,7 +3,53 @@
 include_once 'config.php';
 
 class pQueryTest extends PHPUnit_Framework_TestCase {
+	function test_constructor() {
+		
+	}
 	
+	function test_set_variable() {
+		
+	}
+	
+	function test_getter() {
+		
+	}
+	
+	function test_setter() {
+		
+	}
+	
+	function test_exception_handler() {
+		
+	}
+	
+	function test_extend() {
+		
+	}
+	
+	function test_error() {
+		
+	}
+	
+	function test_load_utils() {
+		
+	}
+	
+	function test_load_plugin() {
+		
+	}
+	
+	function test_require_plugins() {
+		
+	}
+	
+	function test_assert_defined_defined() {
+		
+	}
+	
+	function test_assert_defined_undefined() {
+		
+	}
 }
 
 ?>
